04b3db61498ce7c96e438e482238eee954b3eead
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-18 16:24:56 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan12/aritmatika/IF10K10119912Latihan12Aritmatika.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : Program ini berisi proses aritmarika
+ * 
+ */
+public class IF10K10119912Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));        
+        System.out.println("a - b = " + (a - b));        
+        System.out.println("a * b = " + (a * b));        
+        System.out.println("a / b = " + (a / b));        
+        System.out.println("a % b = " + (a % b));        
+        System.out.println("a++ = " + (a++));        
+        System.out.println("b-- = " + (a--));        
+        System.out.println("d++ = " + (d++));        
+        System.out.println("++d = " + (++d));
+    }
+    
+}
